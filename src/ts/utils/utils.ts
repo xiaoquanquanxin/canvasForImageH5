8af71e98f37ed778a814cb51d6ee924263e3f244
quanxin <quanxin@pujitech.com>
2021-04-06 18:45:44 +0800

    utils.ts

--- src/ts/utils/utils.ts
+++ src/ts/utils/utils.ts
@@ -1,9 +1,14 @@
 //	计算当前图片在canvas中的比例
+import {canvasHeight, canvasWidth} from "@ts/data/device";
+//	获取图片比例
 export const getImageRatio = (img) => {
 	const {width, height} = img;
 	const rw = width / 750;
 	const rh = height / 750;
 	return {
-		rw, rh, width, height
+		rw, rh, width, height,
 	};
 };
+//	检查要画的图在canvas范围内
+export const checkImgInCanvas = (dy: number, dx: number, width: number, height: number): boolean =>
+	!(dy > canvasHeight || dx > canvasWidth || dx < -width || dy < -height);
