cfbb5a2bc5939dc0d533a7f5d114ea31bfdbd4e0
quanxin <quanxin@pujitech.com>
2021-04-06 16:53:42 +0800

    index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import {eventInitFn} from "@ts/event/event"
-import {renderFn} from "@ts/render/render"
-import {imgLoad} from "@ts/event/loadImage"
+import {renderFn} from "@ts/render/render";
+import {playAudio} from "@ts/playAudio/audio";
+import {mediaLoad} from "@ts/event/mediaLoad";
+import {eventInitFn} from "@ts/event/event";
 
 function main() {
 	//	载入资源
-	imgLoad();
-	//	注册事件
-	eventInitFn();
+	mediaLoad();
 	//	绘制
 	renderFn();
+	//	注册事件
+	eventInitFn();
+	//	audio
+	playAudio();
 }
-
 
 main();
 
