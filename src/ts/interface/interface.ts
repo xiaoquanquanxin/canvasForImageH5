3b7679f4284dd4e8ffd4dcb84eecdbaa2de13cb2
quanxin <quanxin@pujitech.com>
2021-04-06 16:53:01 +0800

    interface.ts

--- src/ts/interface/interface.ts
+++ src/ts/interface/interface.ts
@@ -3,7 +3,6 @@ export interface EventInfo {
 	touchStartY: number;
 	currentY: number;
 	inertiaStartY?: number;
-
 	diffY?: number;
 	prevDiffY?: number;
 }
@@ -15,9 +14,39 @@ export interface ImgItem {
 	img: HTMLImageElement;
 	//	用于存储一些额外的信息
 	extra?: any;
+	//	初始Y
+	initiallyY?: number;
 }
 
 //	图片的map
 export interface ImgMap {
-	[any: string]: ImgItem
+	[any: string]: ImgItem;
+
+	airplane: ImgItem;
+	bird: ImgItem;
+	cloud_01: ImgItem;
+	cloud_02: ImgItem;
+	cloud_03: ImgItem;
+	cloud_04: ImgItem;
+	cloud_05: ImgItem;
+	cover: ImgItem;
+	pigeon: ImgItem;
+	year: ImgItem;
+	pigeon_s: ImgItem;
+}
+
+export interface AudioItem {
+	src: string;
+	size: number;
+	name?: string;
+	audio: HTMLAudioElement;
+	//	用于存储一些额外的信息
+	extra?: any;
+}
+
+//	音频的map
+export interface AudioMap {
+	[any: string]: AudioItem;
+
+	bgm: AudioItem;
 }
