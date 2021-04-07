7b453a9505674171904e287a38460a0cb8f2a3c0
quanxin <quanxin@pujitech.com>
2021-04-07 17:40:38 +0800

    打印

--- src/ts/data/device.ts
+++ src/ts/data/device.ts
@@ -16,6 +16,5 @@ export const canvasWidth = window.innerWidth * devicePixelRatio;
 export const canvasHeight = window.innerHeight * devicePixelRatio;
 //	设备宽度比
 export const mainRatio = canvasWidth / 750;
-console.log('mainRatio', mainRatio);
-
-document.getElementById('canvasHeight').innerText = canvasHeight.toString();
+console.log("mainRatio", mainRatio);
+document.getElementById("canvasHeight").innerText = canvasHeight.toString() + "\n" + canvasWidth.toString();
