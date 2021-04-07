ee2a234ee61cad609122f3fae7ba3e3389d92449
quanxin <quanxin@pujitech.com>
2021-04-07 15:26:32 +0800

    上下左右线性移动

--- src/ts/utils/utils.ts
+++ src/ts/utils/utils.ts
@@ -1,5 +1,7 @@
 //	计算当前图片在canvas中的比例
 import {canvasHeight, canvasWidth, mainRatio} from "@ts/data/device";
+import {cacheCtx} from "@ts/data/canvas";
+import {ImgItem} from "@ts/interface/interface";
 //	获取图片比例
 export const getImageRatio = (img) => {
 	const {width, height} = img;
@@ -12,4 +14,20 @@ export const getImageRatio = (img) => {
 //	检查要画的图在canvas范围内
 export const checkImgInCanvas = (dy: number, dx: number, width: number, height: number): boolean => {
 	return !(dy > canvasHeight || dx > canvasWidth || dx < -width * mainRatio || dy < -height * mainRatio);
+};
+//	上下左右线性移动
+export const linearMove = (currentY: number, imgItem: ImgItem) => {
+	const {initX, initY, yK, xK, img} = imgItem;
+	const {rw, rh, width, height} = getImageRatio(img);
+	const dy = (initY + currentY * mainRatio) * yK;
+	const dx = (initX + currentY * mainRatio) * xK;
+	const inCanvas = checkImgInCanvas(dy, dx, width, height);
+	//	优化渲染
+	if (!inCanvas) {
+		return;
+	}
+	cacheCtx.drawImage(img,
+		0, 0, width, height,
+		dx, dy, canvasWidth * rw, canvasWidth * rh,
+	);
 };
