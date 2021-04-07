1932dea9ca07f67472566416db4f3a85cd874e83
quanxin <quanxin@pujitech.com>
2021-04-07 15:51:30 +0800

    renderList

--- src/ts/render/renderList.ts
+++ src/ts/render/renderList.ts
@@ -1,6 +1,6 @@
 import {canvasHeight, canvasWidth, mainRatio} from "@ts/data/device";
 import {cacheCtx} from "@ts/data/canvas";
-import {checkImgInCanvas, getImageRatio} from "@ts/utils/utils";
+import {checkImgInCanvas, getDx, getDy, getImageRatio, linearMove} from "@ts/utils/utils";
 import {ImgItem} from "@ts/interface/interface";
 
 //	静止的，与currentY完全匹配的
@@ -20,112 +20,36 @@ export function renderCover(currentY: number, imgItem: ImgItem) {
 	renderStatic(currentY, img);
 }
 
-//	cloud_03，向左
+//	黑色小云
 export function renderCloud_03(currentY: number, imgItem: ImgItem) {
-	const {initX, initY, yK, xK, img} = imgItem;
-	const {rw, rh, width, height} = getImageRatio(img);
-	const dy = (initY + currentY * mainRatio) * yK;
-	const dx = (initX + currentY * mainRatio) * xK;
-	const inCanvas = checkImgInCanvas(dy, dx, width, height);
-	if (!inCanvas) {
-		return;
-	}
-	cacheCtx.drawImage(img,
-		0, 0, width, height,
-		dx, dy, canvasWidth * rw, canvasWidth * rh,
-	);
+	linearMove(currentY, imgItem);
 }
 
-
-//	cloud_01，向右
-export function renderCloud_01(currentY: number, imgItem: ImgItem) {
-	const {initX, initY, yK, xK, img} = imgItem;
-	const {rw, rh, width, height} = getImageRatio(img);
-	const dy = (initY + currentY * mainRatio) * yK;
-	const dx = (initX + currentY * mainRatio) * xK;
-	const inCanvas = checkImgInCanvas(dy, dx, width, height);
-	if (!inCanvas) {
-		return;
-	}
-	cacheCtx.drawImage(img,
-		0, 0, width, height,
-		dx, dy, canvasWidth * rw, canvasWidth * rh,
-	);
-	//
-	// const {initX, yK, xK, img} = imgItem;
-	// const {rw, rh, width, height} = getImageRatio(img);
-	// const dy = (initX - currentY) * yK;
-	// const dx = (initX + currentY) * xK;
-	// const inCanvas = checkImgInCanvas(dy, dx, width, height);
-	// cacheCtx.drawImage(img,
-	// 	0, 0, width, height,
-	// 	dx, dy, canvasWidth * rw, canvasWidth * rh,
-	// );
-}
-
-
-//	cloud_02，向左
+//	黄色大云
 export function renderCloud_02(currentY: number, imgItem: ImgItem) {
-	const {initX, initY, yK, xK, img} = imgItem;
-	const {rw, rh, width, height} = getImageRatio(img);
-	const dy = (initY + currentY * mainRatio) * yK;
-	const dx = (initX + currentY * mainRatio) * xK;
-	const inCanvas = checkImgInCanvas(dy, dx, width, height);
-	if (!inCanvas) {
-		return;
-	}
-	cacheCtx.drawImage(img,
-		0, 0, width, height,
-		dx, dy, canvasWidth * rw, canvasWidth * rh,
-	);
+	linearMove(currentY, imgItem);
 }
 
-//	cloud_06，向左
+//	白色小云
 export function renderCloud_06(currentY: number, imgItem: ImgItem) {
-	const {initX, initY, yK, xK, img} = imgItem;
-	const {rw, rh, width, height} = getImageRatio(img);
-	const dy = (initY + currentY * mainRatio) * yK;
-	const dx = (initX + currentY * mainRatio) * xK;
-	const inCanvas = checkImgInCanvas(dy, dx, width, height);
-	if (!inCanvas) {
-		return;
-	}
-	cacheCtx.drawImage(img,
-		0, 0, width, height,
-		dx, dy, canvasWidth * rw, canvasWidth * rh,
-	);
-}
-
-//	cloud_04，类背景
-export function renderCloud_04(currentY: number, imgItem: ImgItem) {
-	const {img} = imgItem;
-	currentY += canvasHeight * 1.5;
-	renderStatic(currentY, img);
-}
-
-//	cloud_05，类背景
-export function renderCloud_05(currentY: number, imgItem: ImgItem) {
-	const {img} = imgItem;
-	currentY += canvasHeight * 1.9;
-	renderStatic(currentY, img);
+	linearMove(currentY, imgItem);
 }
 
 //	鸽子，向左
 export function renderPigeon(currentY: number, imgItem: ImgItem, timeout: number) {
-	const {img, extra} = imgItem;
+	const {initX, initY, yK, xK, img, extra} = imgItem;
 	const {rw, rh, width, height} = getImageRatio(img);
-	//	静态的y，是初始化的高度，用于做优化
-	const staticY = canvasHeight * .9;
-	const dx = currentY * 1.1 + staticY;
-	const k = .2;
-	const dy = (staticY - currentY) * k;
+	const dy = getDy(initY, currentY, yK);
+	const dx = getDx(initX, currentY, xK);
 	extra.dx = dx;
 	extra.dy = dy;
+	const inCanvas = checkImgInCanvas(dy, dx, width, height);
+	if (!inCanvas) {
+		return;
+	}
 	const _render = () => {
 		const {dx, dy} = extra;
-		//	console.log(dx, dy);
 		cacheCtx.drawImage(img,
-			//	扇动翅膀
 			timeout % 2 ? width / 2 : 0, 0, width / 2, height,
 			dx, dy, canvasWidth * rw / 2, canvasWidth * rh,
 		);
@@ -133,32 +57,40 @@ export function renderPigeon(currentY: number, imgItem: ImgItem, timeout: number
 	_render();
 }
 
+//	灰色大云，向右
+export function renderCloud_01(currentY: number, imgItem: ImgItem) {
+	linearMove(currentY, imgItem);
+}
+
+//	灰色远处的大云
+export function renderCloud_04(currentY: number, imgItem: ImgItem) {
+	linearMove(currentY, imgItem);
+}
+
 //	飞机，向左
 export function renderAirplane(currentY: number, imgItem: ImgItem) {
+	linearMove(currentY, imgItem);
+}
 
-	const {initX, yK, xK, img} = imgItem;
-	const {rw, rh, width, height} = getImageRatio(img);
-	const dy = (initX - currentY) * yK;
-	const dx = (initX + currentY) * xK;
-	const inCanvas = checkImgInCanvas(dy, dx, width, height);
-	//	优化，不在canvas画布范围
-	if (!inCanvas) {
-		return;
-	}
-	cacheCtx.drawImage(img,
-		0, 0, width, height,
-		dx, dy, canvasWidth * rw, canvasWidth * rh,
-	);
+//	近处的大灰云
+export function renderCloud_05(currentY: number, imgItem: ImgItem) {
+	linearMove(currentY, imgItem);
 }
 
 //	年份
 export function renderYear(currentY: number, imgItem: ImgItem) {
-	const {img} = imgItem;
+	const {initX, initY, yK, xK, img} = imgItem;
 	const {rw, rh, width, height} = getImageRatio(img);
-	const dy = canvasHeight * 2.2 + currentY;
-	const dx = canvasWidth * .3;
+	const dy = getDy(initY, currentY, yK);
+	const dx = getDx(initX, currentY, xK);
+	const inCanvas = checkImgInCanvas(dy, dx, width, height);
+	//	优化渲染
+	if (!inCanvas) {
+		return;
+	}
 	const total = 62;
-	let i = ((-canvasHeight * 1.2 - currentY) / total * 6) | 0;
+	// console.log(canvasHeight - dy | 0);
+	let i = ((canvasHeight - dy) / total * 6) | 0;
 	i = Math.min(i, total - 1);
 	i = Math.max(i, 0);
 	cacheCtx.drawImage(img,
@@ -168,22 +100,24 @@ export function renderYear(currentY: number, imgItem: ImgItem) {
 }
 
 //	小鸽子
-export function renderPigeonSmall(currentY: number, imgItem: ImgItem, timeout) {
-	const {img, extra} = imgItem;
+export function renderPigeonSmall(currentY: number, imgItem: ImgItem, timeout: number) {
+	const {initX, initY, yK, xK, img, extra} = imgItem;
 	const {rw, rh, width, height} = getImageRatio(img);
-	//	静态的y，是初始化的高度，用于做优化
-	const dy = canvasHeight * 2.05 + currentY;
-	const dx = canvasWidth * .6;
+	const dy = getDy(initY, currentY, yK);
+	const dx = getDx(initX, currentY, xK);
+	extra.dx = dx;
 	extra.dy = dy;
+	const inCanvas = checkImgInCanvas(dy, dx, width, height);
+	if (!inCanvas) {
+		return;
+	}
 	const _render = () => {
-		const {dy} = extra;
+		const {dx, dy} = extra;
 		const sx: number = timeout % 3 / 3 * width;
 		cacheCtx.drawImage(img,
-			//	扇动翅膀
 			sx, 0, width / 3, height,
 			dx, dy, canvasWidth * rw / 2, canvasWidth * rh,
 		);
 	};
 	_render();
 }
-
