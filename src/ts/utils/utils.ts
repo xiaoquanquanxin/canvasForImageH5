debbac3d5fb0e886eaf892af4bb900aaa5583bfb
quanxin <quanxin@pujitech.com>
2021-04-07 18:48:44 +0800

    带拐点的移动

--- src/ts/utils/utils.ts
+++ src/ts/utils/utils.ts
@@ -15,6 +15,14 @@ export const getImageRatio = (img) => {
 export const checkImgInCanvas = (dy: number, dx: number, width: number, height: number): boolean => {
 	return !(dy > canvasHeight || dx > canvasWidth || dx < -width * mainRatio || dy < -height * mainRatio);
 };
+//	获取dy
+export const getDy = (initY: number, currentY: number, yK: number): number => {
+	return initY + (currentY * mainRatio) * yK;
+};
+//	获取dx
+export const getDx = (initY: number, currentY: number, xK: number): number => {
+	return initY + (currentY * mainRatio) * xK;
+};
 //	上下左右线性移动 - 基础方法
 export const linearMove = (currentY: number, imgItem: ImgItem) => {
 	const {initX, initY, yK, xK, img} = imgItem;
@@ -31,9 +39,26 @@ export const linearMove = (currentY: number, imgItem: ImgItem) => {
 		dx, dy, canvasWidth * rw, canvasWidth * rh,
 	);
 };
-//	获取dy
-export const getDy = (initY: number, currentY: number, yK: number) => {
-	return initY + (currentY * mainRatio) * yK;
+//	带拐点的移动 - 基础方法
+export const hasInflectionMove = (currentY: number, imgItem: ImgItem) => {
+	let {initX, initY, yK, xK, img, inflexionPointList} = imgItem;
+	const {rw, rh, width, height} = getImageRatio(img);
+	let dy = getDy(initY, currentY, yK);
+	let dx = getDx(initX, currentY, xK);
+	for (const {inflexionPoint, inflexionPointYK, inflexionPointXK} of inflexionPointList) {
+		// console.log(inflexionPoint, inflexionPointYK, inflexionPointXK);
+		if (0 > inflexionPoint + currentY) {
+			dy = getDx(dy, inflexionPoint + currentY, inflexionPointYK - yK);
+			dx = getDx(dx, inflexionPoint + currentY, inflexionPointXK - xK);
+		}
+	}
+	const inCanvas = checkImgInCanvas(dy, dx, width, height);
+	//	优化渲染
+	if (!inCanvas) {
+		return;
+	}
+	cacheCtx.drawImage(img,
+		0, 0, width, height,
+		dx, dy, canvasWidth * rw, canvasWidth * rh,
+	);
 };
-//	获取dx
-export const getDx = getDy;
