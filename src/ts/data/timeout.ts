b7306bb45e4245b384757ae4e6796d9e475390a6
quanxin <quanxin@pujitech.com>
2021-04-13 16:50:01 +0800

    惯性最大最小值

--- src/ts/data/timeout.ts
+++ src/ts/data/timeout.ts
@@ -1,12 +1,13 @@
 //	定时器
 export const timeout = {
-	//	定时器，鸽子飞
-	// pigeon: null,
 	//	总渲染定时器
 	mainRenderTimeout: null,
 	//	总渲染定时器延迟间隔
 	mainRenderTimeoutDelay: 60 * 4,
 	//	滑动惯性定时器，原生requestAnimationFrame回调函数
 	inertia: 0,
+	//	惯性最大最小值
+	maxInertia: 200,
+	minInertia: -200,
 };
 
